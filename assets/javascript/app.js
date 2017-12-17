af2e51f511ebf43798f89a718d5567893c875f7d
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-16 21:28:58 -0600

    questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,28 +22,39 @@ var count = 30;
 
 //make several questions within this object
 var holidayQuestion = [{
-    question: "What Christmas decoration was originally made from strands of silver?",
-    choices: ["Ornaments", "Tinsel", "Mistletoe", "Wreaths"],
-    rightAnswer: 1
+    question: "What does the traditional New Year's song, \"Auld Lang Syne\" mean?",
+    choices: ["Time flies", "Yesterday is gone", "Times gone by", "We have only today"],
+    rightAnswer: 2
+}, {
+    question: "When was the holiday of Kwanzaa introduced?",
+    choices: ["1956", "1966", "1976", "1981"], 
+    rightAnswer: 2
+}, {
+    question: "When was the first New Year celebration?", 
+    choices: ["1000 years ago", "2000 years ago", "3000 years ago", "4000 years ago"],
+    rightAnswer: 3
 }, {
     question: "What beverage company has been using Santa Claus in its advertising since 1931?",
     choices: ["Coca-Cola", "Pepsi", "Dr. Pepper", "Fanta"],
     rightAnswer: 0
-}, {
-    question: "When was the first New Year celebration?", 
-    choices: ["1000 years ago", "2000 years ago", "3000 years ago", "4000 years ago"],
-    rightAnswer: 4
-}, {
-    question: "What does the traditional New Year's song, \"Auld Lang Syne\" mean?"
-    choices: 
-}
-
-// create start button
-//$(".startbutton").click.function(){
-   // $(this).hide();
-   // counter = setInterval(timer, 1000);
-   // displayTrivia();
-//}; 
+} {
+    question: "What Christmas decoration was originally made from strands of silver?",
+    choices: ["Ornaments", "Tinsel", "Mistletoe", "Wreaths"],
+    rightAnswer: 1
+} {
+    question: "What is Hanukkah also known as?",
+    choices: ["Feast of Happiness", "Feast of Dedication", "Feast of Commitment", "Feast of Prosperity"], 
+    rightAnswer: 1
+} {
+    question: "Kwanzaa is a phrase that means \"First Fruits\". What language is this in?", 
+    choices: ["Swahili", "Arabic", "Hausa", "Igbo"], 
+    rightAnswer: 0
+} {
+    question: "How many days long is Hanukkah?"  
+    choices: ["10", "9", "8", "7"],
+    rightAnswer: 2
+} 
+]; 
 
 $(".startbutton").click(function(){
     $(this).hide(); 
