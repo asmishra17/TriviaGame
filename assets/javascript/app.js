b77022eac140b8402b1829fcf6792795be650553
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-16 22:56:28 -0600

    breaking point

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 // psuedocode steps
-// define variables
-// create timer using timeout function
-// initialize game including start screen
+// define variables (!)
+// create timer using interval function (!)
+// initialize game including start screen (!)
 // create on click functions
-// equal to a certain index in the array
 // game conditions
+// next question function
 // reset function
-// game start page
+// game start page (!)
 // game restart page
 
 // global variables
@@ -17,11 +17,10 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var wrongAnswer = 0;
 var question = 0;
-var images;
 var count = 30; 
 
-//make several questions within this object
-var holidayQuestion = [{
+// make several questions within this object
+var holidayQuestions = [{
     question: "What does the traditional New Year's song, \"Auld Lang Syne\" mean?",
     choices: ["Time flies", "Yesterday is gone", "Times gone by", "We have only today"],
     rightAnswer: 2
@@ -37,20 +36,20 @@ var holidayQuestion = [{
     question: "What beverage company has been using Santa Claus in its advertising since 1931?",
     choices: ["Coca-Cola", "Pepsi", "Dr. Pepper", "Fanta"],
     rightAnswer: 0
-} {
+}, {
     question: "What Christmas decoration was originally made from strands of silver?",
     choices: ["Ornaments", "Tinsel", "Mistletoe", "Wreaths"],
     rightAnswer: 1
-} {
+}, {
     question: "What is Hanukkah also known as?",
     choices: ["Feast of Happiness", "Feast of Dedication", "Feast of Commitment", "Feast of Prosperity"], 
     rightAnswer: 1
-} {
+}, {
     question: "Kwanzaa is a phrase that means \"First Fruits\". What language is this in?", 
     choices: ["Swahili", "Arabic", "Hausa", "Igbo"], 
     rightAnswer: 0
-} {
-    question: "How many days long is Hanukkah?"  
+}, {
+    question: "How many days long is Hanukkah?",  
     choices: ["10", "9", "8", "7"],
     rightAnswer: 2
 } 
@@ -58,10 +57,11 @@ var holidayQuestion = [{
 
 $(".startbutton").click(function(){
     $(this).hide(); 
-    var counter = setInterval(timer, 1000); 
+    var counter = setInterval(startTimer, 1000);
+    startTrivia(); 
 })
 
-function timer () {
+function startTimer () {
    count--;
    // why doesn't count = 0 work here
    if (count <= -1) {
@@ -70,6 +70,30 @@ function timer () {
 
     $(".timer").html(`Time Remaining : 00 : ${count} seconds`);
 }
+
+function startTrivia () {
+    $(".question").html(holidayQuestions[0].question);
+    question++;
+
+    var choicesArr = holidayQuestions[0].choices;
+    var buttonsArr = []; 
+
+    for (var i = 0; i < choicesArr.length; i++) {
+        var button = $("<button>");
+        button.text(choicesArr[i]);
+        button.attr("data-id",i);  
+       $ (".answer1").append(button);
+    }
+}
+
+$(".answer1").on("click", "button", function (){
+    userChoice = $(this).data("id"); 
+    holidayQuestions[0].validAnswer;
+    if(userChoice != holidayQuestions[0].validAnswer {
+        $("")
+    }
+})
+
 
 
 
