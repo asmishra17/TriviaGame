c4f941c7115f573895f3461684b70e3581c71b76
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-19 13:46:33 -0600

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,68 +17,70 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var wrongAnswer = 0;
 var question = 0;
-var count = 4; 
+var count = 30; 
+var timerId;
 
 // make several questions within this object
 var holidayQuestions = [{
     question: "What does the traditional New Year's song, \"Auld Lang Syne\" mean?",
     choices: ["Time flies", "Yesterday is gone", "Times gone by", "We have only today"],
-    rightAnswer: 2
+    rightAnswer: 2,
+    wordAnswer: "Times gone by"
 }, {
     question: "When was the holiday of Kwanzaa introduced?",
     choices: ["1956", "1966", "1976", "1981"], 
-    rightAnswer: 2
+    rightAnswer: 1,
+    wordAnswer: "1966"
 }, {
     question: "When was the first New Year celebration?", 
     choices: ["1000 years ago", "2000 years ago", "3000 years ago", "4000 years ago"],
-    rightAnswer: 3
+    rightAnswer: 3,
+    wordAnswer: "4000 years ago"
 }, {
     question: "What beverage company has been using Santa Claus in its advertising since 1931?",
     choices: ["Coca-Cola", "Pepsi", "Dr. Pepper", "Fanta"],
-    rightAnswer: 0
+    rightAnswer: 0,
+    wordAnswer: "Coca-Cola"
 }, {
     question: "What Christmas decoration was originally made from strands of silver?",
     choices: ["Ornaments", "Tinsel", "Mistletoe", "Wreaths"],
-    rightAnswer: 1
+    rightAnswer: 1,
+    wordAnswer: "Tinsel"
 }, {
     question: "What is Hanukkah also known as?",
     choices: ["Feast of Happiness", "Feast of Dedication", "Feast of Commitment", "Feast of Prosperity"], 
-    rightAnswer: 1
+    rightAnswer: 1, 
+    wordAnswer: "Feast of Dedication"
 }, {
     question: "Kwanzaa is a phrase that means \"First Fruits\". What language is this in?", 
     choices: ["Swahili", "Arabic", "Hausa", "Igbo"], 
-    rightAnswer: 0
+    rightAnswer: 0,
+    wordAnswer: "Swahili"
 }, {
     question: "How many days long is Hanukkah?",  
     choices: ["10", "9", "8", "7"],
-    rightAnswer: 2
+    rightAnswer: 2,
+    wordAnswer: "8"
 } 
 ]; 
 
-var timerId;
-
+// start game button
 $(".startbutton").click(function(){
     $(this).hide(); 
-    timerId = setInterval(startTimer, 1000);
+    timerId = setInterval(startTimer, 1000); 
     startTrivia(); 
 })
 
+// timer function 
 function startTimer () {
-   count--;
-   $(".timer").html(`Time Remaining : 00:${count} seconds`);
-   if (count === 0) {
+    count--;
+    $(".timer").html(`Time Remaining : 00:${count} seconds`);
+    if (count === 0) {
         clearInterval(timerId);
         $(".answer1").text("You're out of time!");
+        setTimeout(continueGame, 1500); 
    }; 
 }
-
-//function timeOut () {
-    //if (clearInterval) {
-       // $(".answer1").text("You're out of time!");
-    //}
-//}
-
-
 
 // initialize game
 function startTrivia () {
@@ -97,22 +99,24 @@ function startTrivia () {
     }
 }
 
-// REMEMBER TO RE-.TEXT ANSWER1 for next question
 // game conditions
 $(".answer1").on("click", "button", function (){
-    userChoice = $(this).data("id"); 
-    holidayQuestions[0].rightAnswer;
-    if(userChoice != holidayQuestions[0].rightAnswer) {
-        $(".answer1").text("Wrong answer!"); 
-        incorrectAnswer++;
+        userChoice = $(this).data("id"); 
+        holidayQuestions[0].rightAnswer;
         
-    } else if (userChoice === holidayQuestions[0].rightAnswer) {
-        $(".answer1").text("Correct!"); 
-        correctAnswers++;  
-    }    
+        if(userChoice != holidayQuestions[0].rightAnswer) {
+            $(".answer1").text(`Wrong answer! The answer was: \"${holidayQuestions[0].wordAnswer}\"`); 
+            wrongAnswer++;
+            clearInterval(timerId);
+            
+        
+        } else if (userChoice === holidayQuestions[0].rightAnswer) {
+            $(".answer1").text("Correct!"); 
+            correctAnswers++;  
+            clearInterval(timerId);
+             
+        }   
 });
-
-
 
 
 // game continuation function
@@ -121,12 +125,18 @@ function continueGame () {
         question++;
 
         choicesArr = holidayQuestions[1].choices;
+        count = 30;
+        timerId = setInterval(startTimer, 1000);
+        $ (".answer1").text(" ");
         
+
         for (var i = 0; i < choicesArr.length; i++) {
+            var button = $("<button>");
             button.text(choicesArr[i]);
             button.attr("data-id",i); 
             $ (".answer1").append(button); 
         }
+
     }
 
 
